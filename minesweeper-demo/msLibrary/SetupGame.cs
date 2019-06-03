fe31b2ff9a849365c8ffc2481a6720a2187192f4
Luis Camacho <lc@backlayer.com.pa>
2019-06-02 22:13:56 -0500

    Main objects for game setup

---
+++ minesweeper-demo/msLibrary/SetupGame.cs
@@ -0,0 +1,66 @@
+﻿using msLibrary.setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static msLibrary.setup.GameVariables;
+
+namespace msLibrary
+{
+    /// <summary>
+    /// Main class for game setup
+    /// </summary>
+    public class SetupGame
+    {
+
+        #region Methods
+
+        /// <summary>
+        /// Loads all the setup for the game to play acording to the parameter
+        /// </summary>
+        /// <param name="SelectedMode"></param>
+        /// <returns>Game setup</returns>
+        public static GameSetting LoadGameSetup(string SelectedMode)
+        {
+            #region variables
+            //Validate the Selected game mode
+            GameplayMode _selectedMode = ValidateGameMode(SelectedMode);
+
+            
+            //Settings to be returned in LoadGame Method
+            GameSettingObj = new GameSetting();
+            #endregion
+
+            //Load Gamemode settings
+            switch (_selectedMode)
+            {
+                case GameplayMode.easy:
+                    GameSettingObj.xAxis = EasyModeSettings.xAmount;
+                    GameSettingObj.yAxis = EasyModeSettings.yAmount;
+                    GameSettingObj.mines = EasyModeSettings.mineAmount;
+                    break;
+                default:
+                    throw new Exception("Error");
+            }
+
+            return GameSettingObj;
+        }
+        
+        /// <summary>
+        /// Validate Game mode against the enum
+        /// </summary>
+        /// <param name="selectedMode"></param>
+        /// <returns>String for game mode</returns>
+        private static GameplayMode ValidateGameMode(string selectedMode)
+        {
+            GameplayMode recordType = new GameplayMode();
+            
+            recordType = (GameplayMode)System.Enum.Parse(typeof(GameplayMode), selectedMode);
+            
+            return recordType;
+        }
+        
+        #endregion
+    }
+}
---
+++ minesweeper-demo/msLibrary/setup/GameVariables.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msLibrary.setup
+{
+    public class GameVariables
+    {
+        /// <summary>
+        /// Supported Game Modes
+        /// </summary>
+        public enum GameplayMode { easy }
+        
+
+        /// <summary>
+        /// Static object for GameSetting
+        /// </summary>
+        public static GameSetting GameSettingObj { get; set; }
+        
+        /// <summary>
+        /// Main attributes for a setting
+        /// </summary>
+        public class GameSetting
+        {
+            public int xAxis { get; set; }
+            public int yAxis { get; set; }
+            public int mines { get; set; }
+        }
+
+
+        /// <summary>
+        /// Easy Mode Settings
+        /// </summary>
+        public static class EasyModeSettings
+        {
+            public const int xAmount = 9;
+            public const int yAmount = 9;
+            public const int mineAmount = 10;
+
+        }
+
+
+    }
+}
