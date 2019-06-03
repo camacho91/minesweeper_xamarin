d5f3ab9af11e4e3db3c732e484b9a88932a4abe3
Luis Camacho <lc@backlayer.com.pa>
2019-06-02 22:37:59 -0500

    Main resources and initial test layout.
    All visual resources added.
    Test layout with GridView as main control for the game.
    Custom cell to be populated in GridView.
    Theme changed to hide Action Bar.

--- minesweeper-demo/deviget-minesweeper/MainActivity.cs
+++ minesweeper-demo/deviget-minesweeper/MainActivity.cs
@@ -9,11 +9,27 @@ namespace deviget_minesweeper
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+
+        #region Variables
+
+        //GrigView container for the game board
+        GridView gridGame;
+
+        #endregion
+
+
+        #region methods
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            //Grid initialization
+            gridGame = FindViewById<GridView>(Resource.Id.gridGame);
+
         }
+        #endregion
     }
 }
